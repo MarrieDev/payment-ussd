95a8b9e98b4ee453844f634f190b720a3ded51d6
Marriane791 <annemarrieakeyo42@gmail.com>
2022-05-10 16:55:48 +0300

    make it better

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <?php
-//Read variables sent via POST from AT gateway
+// Read the variables sent via POST from our API
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
-if ($text == ""){
-    $response = "CON what would you want to check \n";
-    $response = "1.My Account \n";
-    $response = "2. My PhoneNumber";
-}elseif ($text == "1") {
-    $response = "CON choose the information you want to veiw \n";
-    $response = "1.Account Number \n";
-    $response = "2.Account Balance";
-}elseif ($text == "2") {
-    $response = "END Your phone number is ".$phoneNumber ;
-}elseif ($text == "1*1") {
-    # static account number 
-    $accountNo = "WERT3456FGH";
-    $response = "END Your account number is ".$accountNo;
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 
-}elseif ($text == "1*2") {
-    # static account balance 
-    $balance = "sh. 15000";
-    $response = "END Your account balance is ".$balance;
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
 
 }
-header('Content-type:text/plain');
+
+// Echo the response back to the API
+header('Content-type: text/plain');
 echo $response;
-?> 
